9b0c0550bf6d3b1db112908d2272bcf376f3998c
SelviIndriani <selvindri1010@gmail.com>
2023-12-08 21:29:05 +0700

    commmit tht8

---
+++ weak 8/takehometask8/pesan.php
@@ -0,0 +1,13 @@
+<?php
+$nama = 'Candy';
+$produk = array(
+  array("Skintific" , 120000),
+  array("Emina" , 75000),
+  array("Avoskin" , 300000),
+  array("MSglow" , 300000),
+  array("Lacoco" , 400000),
+  array("Somethinc" , 420000),
+  array("Daviena" , 280000),
+  array("Cosrx" , 450000),
+)
+?>
